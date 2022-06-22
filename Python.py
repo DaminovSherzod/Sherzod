7ae09a3954d2a67b75b89bde98db3e17cde37d22
Sherzod <daminovsherzod2002@gmail.com>
2022-06-22 17:05:15 +0500

    Python.py

--- Python.py
+++ Python.py
@@ -1,3 +1,14 @@
-def main():
-    return 0
-print(main()) 
+
+a = 3
+b = 4
+c = 5
+if a>b:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b>c:
+        print(b)
+    else:
+        print(c)                 
